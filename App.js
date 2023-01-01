d940c9666dab90f25c02872d4194142385e9c1cc
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-01-01 11:33:50 +0530

    carry form class 2

---
+++ App.js
@@ -0,0 +1,37 @@
+
+/**
+ * Parcel:
+ * HMR- Hot module Replacement 
+ * HMR works with File watcher Algorithm - written in C++
+ * Parcel is bundling, minifying and cleaning the code(removing console)
+ * Parcel optimize project inside image also
+ * CACHING while development. not in production
+ * Compatible with older version of browser
+ * HTTPS  on dev = npx parcel index.html --https
+ * PORT management
+ * Consistent Hashing Algorithm
+ * ZERO Config
+ * 
+ * 
+ * Transitive Dependencies.
+ * 
+ */
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading1 = React.createElement("h1", {
+  id: "title" //this is props object which is the properties of a dom
+}, "Heading 1")
+
+const heading2 = React.createElement("h2", {
+  id: "description" //this is props object which is the properties of a dom
+}, "Heading 2")
+
+const heading = React.createElement("div", {
+  id: "some", //this is props object which is the properties of a dom
+  name: "some name"
+}, [heading1, heading2])
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(heading)
---
+++ class3.md
@@ -0,0 +1,22 @@
+# **Revision of class 2:**
+
+> Parcel is use for small production level projects/ teaching.
+
+> Some key adv. are -
+ * HMR- Hot module Replacement 
+ * HMR works with File watcher Algorithm - written in C++
+ * Parcel is bundling, minifying and cleaning the code(removing console)
+ * Parcel optimize project inside image also
+ * CACHING while development. not in production
+ * Compatible with older version of browser
+ * HTTPS  on dev = npx parcel index.html --https
+ * PORT management
+ * Consistent Hashing Algorithm
+ * ZERO Config
+ * Tree Shaking - Removing un-wanter
+ * 
+ * Transitive Dependencies.
+
+> npx parcel index.html  or   npm parcel --build index.html
+
+# **Class 3**
