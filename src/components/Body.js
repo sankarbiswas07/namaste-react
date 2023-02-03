4a2d87844c71ffaa7bcda709aa13866e9a95a003
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-02-04 01:59:06 +0530

    changes: searchBar(only design) is in nav bar, cutom hook: useRestaurants [remove: search functionality]

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,15 @@
-import "../../style.css"
-// import { restaurantList } from "../constants"
 import { useState, useEffect } from "react"
+import useRestaurants from "../hooks/useRestaurants"
 import RestaurantCard from "./RestaurantCard"
 import { RestaurantCardSkeleton } from "./Shimmer"
 
 const Body = () => {
   const [searchText, setSearchText] = useState("")
-  const [allRestaurants, setAllRestaurants] = useState([])
   const [filteredRestaurants, setFilteredRestaurants] = useState([])
 
-  useEffect(() => { getApiData() }, [])
+  const allRestaurants = useRestaurants()
+  // setFilteredRestaurants(allRestaurants)
+  console.table(allRestaurants)
 
   // A Few cases
   // not render component (Early return)
@@ -26,44 +26,12 @@ const Body = () => {
     )
   }
 
-  async function getApiData() {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
-    )
-    const json = await data.json()
-    // Optional Chaining
-    const cardData = json?.data?.cards[2]?.data?.data?.cards
-    console.log(cardData)
-    setAllRestaurants(cardData);
-    setFilteredRestaurants(cardData);
-  }
-
   return (
     <>
-      {/* search section */}
-      <div className="search-bar">
-        <input type="text"
-          className="search-input"
-          placeholder="Restaurant name / cuisine"
-          onChange={(e) => { setSearchText(e.target.value) }}
-          value={searchText} />
-
-        <div
-          className="search-btn search-height"
-          onClick={() => {
-            return setFilteredRestaurants(allRestaurants.filter(restaurant => {
-              return restaurant?.data?.name?.toLowerCase().includes(searchText?.trim()?.toLowerCase())
-                || restaurant?.data?.cuisines?.filter(e => e?.trim()?.toLowerCase()?.includes(searchText?.trim()?.toLowerCase())).length
-            }
-            ))
-          }}>Search</div>
-      </div>
-
-
       {/* list section */}
       <div className="flex flex-wrap">
         {
-          filteredRestaurants.map(restaurant => {
+          allRestaurants.map(restaurant => {
             return (<RestaurantCard {...restaurant.data} key={restaurant.data.id} />)
           })
         }
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,18 +7,50 @@ const Header = () => {
   const [searchText, setSearchText] = useState()
 
   return (
-    <div className="flex h-14 bg-blue-400 justify-between mb-6">
-      <div className="brand flex items-center">
-        {/* <img src="https://w7.pngwing.com/pngs/55/100/png-transparent-swiggy-hd-logo-thumbnail.png" /> */}
-        <h3> FoodVilla</h3>
-        <small>{searchText}</small>
+    <div className="h-14  bg-blue-400 mb-6">
+      <div className="w-[1024] flex justify-between m-auto  ">
+        <div className="brand flex items-center">
+          {/* <img src="https://w7.pngwing.com/pngs/55/100/png-transparent-swiggy-hd-logo-thumbnail.png" /> */}
+          <h3> FoodVilla</h3>
+          <small>{searchText}</small>
+        </div>
+        {/* search section */}
+        <div className="flex items-center ">
+          {/* <input type="text"
+          className="inp"
+          placeholder="Restaurant name / cuisine"
+          onChange={(e) => { setSearchText(e.target.value) }}
+          value={searchText} /> */}
+          <label className="relative block">
+            <span className="sr-only">Search</span>
+            {/* <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+            <svg class="h-5 w-5 fill-slate-300" viewBox="0 0 20 20">Q</svg>
+          </span> */}
+            <input className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+              placeholder="Search for anything..."
+              type="text"
+              onChange={(e) => { setSearchText(e.target.value) }}
+              value={searchText}
+              name="search" />
+          </label>
+
+          <div
+            className="ml-2 h-9 pl-3 pr-3 items-center bg-yellow-400 rounded-md"
+            onClick={() => {
+              return setFilteredRestaurants(allRestaurants.filter(restaurant => {
+                return restaurant?.data?.name?.toLowerCase().includes(searchText?.trim()?.toLowerCase())
+                  || restaurant?.data?.cuisines?.filter(e => e?.trim()?.toLowerCase()?.includes(searchText?.trim()?.toLowerCase())).length
+              }
+              ))
+            }}>Search</div>
+        </div>
+        <ul className="side-menu flex items-center">
+          <li className="pl-2">Offers</li>
+          <li className="pl-2">Help</li>
+          <li className="pl-2">Sankar P...</li>
+          <li className="pl-2">Cart</li>
+        </ul>
       </div>
-      <ul className="side-menu flex items-center">
-        <li className="pl-2">Offers</li>
-        <li className="pl-2">Help</li>
-        <li className="pl-2">Sankar P...</li>
-        <li className="pl-2">Cart</li>
-      </ul>
     </div>
   )
 }
